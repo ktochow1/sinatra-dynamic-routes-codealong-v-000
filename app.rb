1e27889a5616fae75c6592c5103908dc9e22d73c
Kat Tochowicz <ktochow1@binghamton.edu>
2019-04-24 17:08:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,9 +22,9 @@ class App < Sinatra::Base
   end
   
   get '/multiply/:num1/:num2' do
-    # @num1 = params[:num1]
+    @num1 = params[:num1]
     
-    # @num2 = params[:num2]
+    @num2 = params[:num2]
     "#{params[:num1] * params[:num2]}"
   end
   binding.pry
